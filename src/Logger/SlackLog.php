4a889287ef4f9d89af011e43802bcc4388650f0c
Michiel Huijse <michiel.huijse@gmail.com>
2019-02-27 21:46:29 +0100

    Proof of concept using the logger service.

---
+++ src/Logger/SlackLog.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\slack_log\Logger;
+
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+/**
+ * SlackLogger controller.
+ */
+class SlackLog implements LoggerInterface {
+    use RfcLoggerTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function log($level, $message, array $context = []) {
+
+        $config = \Drupal::config('slack.settings');
+        $channel = $config->get('slack_channel');
+        $username = $config->get('slack_username');
+        $configSlackLog = \Drupal::config('slack_log.settings');
+        $severity = $configSlackLog->get('severity');
+
+      $response = \Drupal::service('slack.slack_service')->sendMessage($message, $channel, $username);
+
+      if ($response && RedirectResponse::HTTP_OK == $response->getStatusCode()) {
+          drupal_set_message(t('Message was successfully sent!'));
+      }
+      else {
+          drupal_set_message(t('Please check log messages for further details'), 'warning');
+      }
+    }
+}
